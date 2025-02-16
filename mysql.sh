335ec7b4af16b2e38c2f817fe1a20863ebc90197
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-02-16 17:28:57 +0530

    expense-project

---
+++ mysql.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+then
+    echo "Please run the script root priveleges" | tee -a $LOG_FILE
+    exit 1
+fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is...$G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed...going to install"
+    dnf install mysql-server -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "MySQL is already installed..nothing to do"
+fi
+
+systemctl enable mysqld
+VALIDATE $? "Enabled mysql server"
+
+systemctl start mysqld
+VALIDATE $? "Started mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting up the password"
